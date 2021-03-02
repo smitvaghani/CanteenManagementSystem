e8dd3c86415119de9afb264ff96a2fec0e9333b6
Your Name <you@example.com>
2021-03-02 20:00:03 +0530

    Lab8 Loginmodule

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MyProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ loginmodule/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.template.context_processors import csrf
+
+# Create your views here.
+
+# def login(request):
+#     if request.method == 'POST':
+#         username= request.POST['username']
+#         password= request.POST['password']
+#         user = auth.authenticate(username=username,password=password)
+#         if user is not None:
+#             auth.login(request,user)
+#             messages.info(request, "you have successfully logged in")
+#             return render(request, 'login.html')
+#         else:
+#             messages.info(request, 'your username or password is invalid!')
+#             return render(request, 'login.html')
+#     else:
+#         return render(request, 'login.html')
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render(request,'login.html')
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username,password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/loginmodule/loggedin/')
+    else:
+        return HttpResponseRedirect('/loginmodule/invalidlogin/')
+
+def loggedin(request):
+    return render(request,'loggedin.html', {"full_name":request.user.username})
+
+
+def invalidlogin(request):
+    return render(request,'invalidlogin.html')
+
+def logout(request):
+    auth.logout(request)
+    return render(request,'logout.html')
+

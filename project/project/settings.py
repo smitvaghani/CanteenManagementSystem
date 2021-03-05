72d7040211128a9a47db1da647f6315dbab494f2
Smit <smitvaghani3343@gamil.com>
2021-03-05 14:07:35 +0530

    login change commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'canteen',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
+        'USER': 'root',
+        'PASSWORD': 'Ss#21476',
         'HOST': 'localhost',
-        'PORT': '5432'
+        'PORT': '3306',
     }
 }
 
@@ -125,11 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
